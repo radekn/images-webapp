b4c4203ee32ce1b6eb2dee6b988775a2bd8877a5
Radoslaw Nadstawny <radoslaw.nadstawny@gmail.com>
2020-07-19 03:48:04 +0200

    Create a React app fetching sample data from API

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,5 +1,6 @@
 import Koa from "koa";
 import Router from "@koa/router";
+import cors from "@koa/cors";
 
 const app = new Koa();
 const router = new Router();
@@ -8,6 +9,7 @@ router.get("/hello", (ctx) => {
   ctx.body = "Hello there!";
 });
 
+app.use(cors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(process.env.PORT ?? 8000);
--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,5 +1,24 @@
 import "./index.html";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
 declare const API_URL: string;
 
-console.log("Hello there!");
-console.log("API_URL is", API_URL);
+const App: React.FunctionComponent = () => {
+  const [response, setResponse] = useState<string>();
+  useEffect(() => {
+    fetch(`${API_URL}/hello`).then(async (res) => {
+      if (res.ok) {
+        setResponse(await res.text());
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      API says: <pre>{response}</pre>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("react-root"));

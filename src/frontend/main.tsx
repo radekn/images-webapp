aa2476dfd117069ae188bd003f1462d2b927b761
Radoslaw Nadstawny <radoslaw.nadstawny@gmail.com>
2020-07-19 02:53:46 +0200

    Set up frontend build with Webpack

---
+++ src/frontend/main.tsx
@@ -0,0 +1,5 @@
+import "./index.html";
+declare const API_URL: string;
+
+console.log("Hello there!");
+console.log("API_URL is", API_URL);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+"use strict";
+const webpack = require("webpack");
+const path = require("path");
+
+const resolve = (name) => path.resolve(__dirname, name);
+
+module.exports = {
+  context: resolve("src/frontend"),
+  entry: {
+    main: "./main.tsx",
+  },
+  output: {
+    path: resolve("dist/frontend"),
+  },
+  resolve: {
+    alias: {
+      src: resolve("src"),
+    },
+    modules: [resolve("node_modules")],
+    extensions: [".ts", ".tsx", ".mjs", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["file-loader?name=[path][name].[ext]"],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: resolve("node_modules"),
+        use: ["ts-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      API_URL: JSON.stringify(process.env.API_URL || "http://localhost:8000"),
+    }),
+  ],
+  devServer: {
+    contentBase: false,
+    serveIndex: false,
+  },
+};
